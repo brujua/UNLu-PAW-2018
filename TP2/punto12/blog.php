c970bc2953f1319401609dd947ad075145a98062
brujua <brunocrisafulli@yahoo.com.ar>
2018-03-31 18:21:42 -0300

    Punto 12 sin terminar

---
+++ TP2/punto12/blog.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brujua
+ * Date: 31/3/2018
+ * Time: 5:54 PM
+ */
+
---
+++ TP2/punto12/navigate.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brujua
+ * Date: 31/3/2018
+ * Time: 6:20 PM
+ */
