6bae6ff72f2bdf86db1a43554a25921c19fe82ac
brujua <brunocrisafulli@yahoo.com.ar>
2018-04-06 03:33:50 -0300

    Punto 12 Crear y Ver post andando

--- TP2/punto12/verPosts.php
+++ TP2/punto12/verPosts.php
@@ -1,8 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: brujua
- * Date: 5/4/2018
- * Time: 2:23 AM
- */
 
+
+require_once "utils.php";
+
+echo blogStart();
+
+echo "<main>";
+
+
+$doc = new DOMDocument();
+$doc->load("posts.xml");
+$xml = $doc->getElementsByTagName("xml")->item(0);
+$posts = $xml->getElementsByTagName("post");
+for ($i = 0; $i < $posts->length; $i++) {
+    echo "<article>";
+    $imgName = null; // variable usada para guardar las direcciones de las imagenes
+    $imgNode = $posts->item($i)->getElementsByTagName("imgName");
+    //Si el post tenia imagen, devolverá un nodo
+    if ($imgNode->length > 0) {
+        $imgName = $imgNode->item(0)->nodeValue;
+        echo "<img src=\"imgs\\";
+        echo "$imgName";
+        echo "\" alt=\"\">";
+    }
+    //recupero fecha
+    $fechaNode = $posts->item($i)->getElementsByTagName("date");
+    $fecha = $fechaNode->item(0)->nodeValue;
+    //recupero titulo
+    $titleNode = $posts->item($i)->getElementsByTagName("title");
+    $title = $titleNode->item(0)->nodeValue;
+    //recupero descripcion
+    $descNode = $posts->item($i)->getElementsByTagName("descrp");
+    $desc = $descNode->item(0)->nodeValue;
+
+    echo " <h2> $title</h2>
+        <p> $desc</p>
+        <small> $fecha</small> 
+        </article>";
+
+} // fin for
+
+echo "</main>";
+
+echo blogEnd();
