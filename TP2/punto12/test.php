22ab54cd3840da11dadd5c2da627469a4d30982b
brujua <brunocrisafulli@yahoo.com.ar>
2018-05-17 05:33:26 -0300

    Terminado estilo pagina E-Commerce

---
+++ TP2/punto12/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brujua
+ * Date: 15/5/2018
+ * Time: 5:02 PM
+ */
+
+require_once 'utils.php';
+$var = 'hola,como, estas';
+
+print_r(explode(',',$var)) ;
