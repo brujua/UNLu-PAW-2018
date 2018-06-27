483fe07156c5f9923d3ce30c874ae7d579e45ddb
MarioQuiroga <marioquiro17@gmail.com>
2018-06-27 13:37:59 -0300

    iniciando proyecto final

--- TP5/Punto 8/index.php
+++ TP5/Punto 8/index.php
@@ -24,7 +24,7 @@
 	<footer>
 		
 	</footer>
-	<script type="text/javascript"  src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript"  src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>	
 	<script type="text/javascript" src="js/verificarLinks.js"></script>
 </body>
 </html>
